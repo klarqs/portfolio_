42c280050460fe608a45a80b3fd1afe4092c2fd0
Kolawole Paul Oluwafemi <pauljacksonjr@Pauls-MacBook-Pro.local>
2022-04-05 19:32:54 +0100

    update on app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:lots_/app/modules/transaction_history/views/transcation_history_view.dart';
-import 'package:lots_/app/utils/colors.dart';
 import 'package:provider/provider.dart';
 
 import 'app/utils/ui/theme_manager.dart';
 
 void main() {
-  return runApp(ChangeNotifierProvider<ThemeNotifier>(
-    create: (_) => ThemeNotifier(),
-    child: MyApp(),
-  ));
-  ;
+  return runApp(
+    ChangeNotifierProvider<ThemeNotifier>(
+      create: (_) => ThemeNotifier(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
